a1810eb3e8b248e328911436652765f141da1dcd
张黎 <zhangli@lehoolive.com>
2017-08-28 13:04:53 +0800

    update code

--- upload_file.php
+++ upload_file.php
@@ -40,9 +40,9 @@ if ($_FILES["upload_file"]["error"] > 0) {
     move_uploaded_file($_FILES["upload_file"]["tmp_name"], "upload/" . $_FILES["upload_file"]["name"]);
     echo "Stored in: " . "upload/" . $_FILES["upload_file"]["name"] . "<br/>";
 
-    // If file size less than 1MB, display base64 content.
+    // If file size less than 150k, display base64 content.
     $filename = "upload/" . $_FILES["upload_file"]["name"];
-    if ($_FILES["upload_file"]["size"] / 1024 <= 1024) {
+    if ($_FILES["upload_file"]["size"] / 1024 < 150) {
       $imgbinary = fread(fopen($filename, "r"), filesize($filename));
       echo base64_encode($imgbinary);
     }
